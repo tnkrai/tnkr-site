1c849ca0f4aed2ff0ace01b5efe696f803d030ec
JohnOlushola <johntemiloluwa.o@gmail.com>
2024-06-01 14:50:31 +0100

    feat: `Button` font-sans

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium capitalize transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-sans font-medium capitalize transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
