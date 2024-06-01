88a31eb76af1221907e62e87dc4db0c1f3ca7a87
JohnOlushola <johntemiloluwa.o@gmail.com>
2024-06-01 14:14:58 +0100

    feat: `Button` + hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Button from "@/components/ui/Button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen gap-16 flex-col p-24">
+      <h1>
+        Tnkr.ai is a tool for exploring the source knowledge of any technology.
+        We believe that every piece of technology, old or new, has a unique
+        story within it. We want to help the next generation of tinkerers
+        uncover these stories so new ones can be told.
+      </h1>
+
+      <div>
+        <Button>Join Our Waitlist</Button>
+      </div>
+    </main>
   );
 }
