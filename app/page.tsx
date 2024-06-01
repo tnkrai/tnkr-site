916706b624d28baa6fc79cbc2294ddcb1c910035
JohnOlushola <johntemiloluwa.o@gmail.com>
2024-06-01 14:19:53 +0100

    feat: `Open Source Robots - Idea`

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,38 @@ export default function Home() {
       <div>
         <Button>Join Our Waitlist</Button>
       </div>
+
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <h1 className="mb-2">
+            Open Source Robots - <span className="text-green-500">Idea</span>
+          </h1>
+          <p>
+            As soon as we set off to build tinkr, our first instinct was to find
+            a group of people who could benefit “the most” from having a tool
+            like Tinkr. To ensure we were looking in the right places, we
+            established 3 conditions for a given niche to be considered an ideal
+            segment for us to plant our roots.
+          </p>
+        </div>
+
+        <ol className="list-decimal">
+          <li>
+            There had to be a considerable number of individuals / entities,
+            willing to share the knowledge within technology they had built (
+            open source ).
+          </li>
+          <li>
+            There had to be a considerable number of individuals / entities who
+            are highly curious about this technology and want to tinker with it,
+            tear it down and create new things from it.
+          </li>
+          <li>
+            Problems and inadequacies in the current process of sharing and
+            tinkering with the open source knowledge.
+          </li>
+        </ol>
+      </div>
     </main>
   );
 }
