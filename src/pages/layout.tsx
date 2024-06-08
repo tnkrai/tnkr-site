632f5380544debdc736695c4f62584cdf2f813ba
Joseph Olabisi <dabigjoe6@gmail.com>
2024-06-08 11:40:42 +0100

    pages-nextjs-default-routing

---
+++ src/pages/layout.tsx
@@ -0,0 +1,16 @@
+import { Source_Serif_4 } from "next/font/google";
+
+const sourceSerif = Source_Serif_4({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const RootLayout = ({ children }: Readonly<{children: React.ReactNode; }>) => {
+  return (
+    <>
+      <div className={sourceSerif.className}>{children}</div>
+    </>
+  )
+}
+
+export default RootLayout;
