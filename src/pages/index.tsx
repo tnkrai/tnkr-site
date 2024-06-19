c4a11c9e6ddb770ad25668e5db753b5fb83cebe7
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-06-19 22:55:28 +0100

    feat: content width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
         src:
           `http://${process.env.NEXT_PUBLIC_CMS_HOST}` + image?.attributes?.url,
         alt: image?.attributes?.alternativeText,
-      }),
+      })
     );
 
   const fallbackImages = [
@@ -59,8 +59,8 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <main className="flex min-h-screen gap-16 flex-col">
       <div>
-        <h1>{heading ?? fallbackHeading}</h1>
-        <p>{subheading ?? fallbackSubheading}</p>
+        <h1 className="max-w-md">{heading ?? fallbackHeading}</h1>
+        <p className="mt-2 max-w-xl">{subheading ?? fallbackSubheading}</p>
       </div>
       <section className=".cta">
         <Button url={ctaButtonUrl ?? fallbackCtaButtonUrl}>
@@ -68,7 +68,7 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
         </Button>
       </section>
       <div className="flex flex-col gap-y-4">
-        <div>
+        <div className="max-w-xl">
           <h1 className="mb-5">
             <span>{bodyHeading ?? fallbackBodyHeading}</span>
             <span className="text-green-500">
@@ -77,7 +77,8 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
           </h1>
           <p>{bodyText ?? fallbackBodyText}</p>
         </div>
-        <ol className="list-decimal mb-10">
+
+        <ol className="list-decimal mb-10 max-w-xl">
           {bodyList ? (
             bodyList.map(
               (
@@ -87,11 +88,11 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
                     text: string;
                   }>;
                 },
-                index: number,
+                index: number
               ) => {
                 const text = listItem?.children[0]?.text;
                 return <li key={index}>{text}</li>;
-              },
+              }
             )
           ) : (
             <>
@@ -112,6 +113,7 @@ const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
             </>
           )}
         </ol>
+
         <ImageGroup images={images ?? fallbackImages} />
       </div>
     </main>
@@ -149,7 +151,7 @@ export const getStaticProps: GetStaticProps<{ data: HomeData }> = async () => {
       `http://${process.env.CMS_HOST}/api/landing?populate[0]=landing_body.landing_images.body_media`,
       {
         headers,
-      },
+      }
     );
 
     data = await res.json();
