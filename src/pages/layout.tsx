f5105589bdc8656acf9cc80f8445146a2c10a334
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-06-19 22:27:31 +0100

    feat: clean up pages

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,5 @@
 import { Source_Serif_4 } from "next/font/google";
-
-import { ThemeProvider } from "@contexts/Theme";
-
 import classNames from "classnames";
-
-// import Footer from "@components/Footer";
 import Footer from "../components/Footer";
 
 const sourceSerif = Source_Serif_4({
@@ -14,17 +9,15 @@ const sourceSerif = Source_Serif_4({
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <ThemeProvider>
-      <div
-        className={classNames(
-          `mx-auto-px-3 py-6 md:px-6 md:py-10 lg:px-4 xl:py-10`,
-          sourceSerif.className,
-        )}
-      >
-        {children}
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div
+      className={classNames(
+        `mx-auto-px-3 py-6 md:px-6 md:py-10 lg:px-4 xl:py-10`,
+        sourceSerif.className
+      )}
+    >
+      {children}
+      <Footer />
+    </div>
   );
 };
 
