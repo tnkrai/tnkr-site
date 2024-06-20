42830cb72f6019f264e127d762f3e5209a20f53e
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-06-20 10:48:30 +0100

    fix: header width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@ import Button from "./ui/Button";
 
 export default function Header() {
   return (
-    <header className="fixed w-full flex justify-between items-center py-2.5 border-b bg-black container">
-      <Link href="/">tnkr.ai</Link>
+    <header className="fixed w-full border-b bg-black py-2.5 ">
+      <div className="flex justify-between items-center container">
+        <Link href="/">tnkr.ai</Link>
 
-      <Button asChild size="sm" variant="outline">
-        <a href="https://discord.gg/fcpeKMKn3E" target="_blank">
-          Join Our Discord
-        </a>
-      </Button>
+        <Button asChild size="sm" variant="outline">
+          <a href="https://discord.gg/fcpeKMKn3E" target="_blank">
+            Join Our Discord
+          </a>
+        </Button>
+      </div>
     </header>
   );
 }
