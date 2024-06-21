3eff68763dd23121a74e7b900b510c19d6a49b2c
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-06-21 14:52:42 +0100

    feat: video support + update content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,121 +1,7 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Button from "../components/ui/Button";
-import ImageGroup from "../components/ImageGroup";
-import Layout from "./layout";
+import MediaGroup from "@components/MediaGroup";
 
-const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const heading = data?.attributes?.heading;
-  const fallbackHeading =
-    "Tnkr.ai is a tool for exploring the source knowledge of any technology.";
-  const subheading = data?.attributes?.subheading;
-  const fallbackSubheading =
-    "We believe that every piece of technology, old or new, has a unique story within it. We want to help the next generation of tinkerers uncover these stories so new ones can be told.";
-
-  const ctaButtonTitle = data?.attributes?.cta?.button?.title;
-  const fallbackCtaButtonTitle = "Join Our Discord";
-  const ctaButtonUrl = data?.attributes?.cta?.button?.url;
-  const fallbackCtaButtonUrl = "https://discord.gg/fcpeKMKn3E";
-
-  const bodyHeading = data?.attributes?.landing_body?.heading;
-  const fallbackBodyHeading = "Open Source Robots - ";
-  const bodySubheading = data?.attributes?.landing_body?.sub_heading;
-  const fallbackBodySubheading = "Idea";
-
-  const bodyText = data?.attributes?.landing_body?.body[0]?.children[0]?.text;
-  const fallbackBodyText =
-    "As soon as we set off to build tinkr, our first instinct was to find a group of people who could benefit “the most” from having a tool like Tinkr. To ensure we were looking in the right places, we established 3 conditions for a given niche to be considered an ideal segment for us to plant our roots.";
-  const bodyList = data?.attributes?.landing_body?.body[1]?.children;
-
-  const images =
-    data?.attributes?.landing_body?.landing_images?.body_media?.data.map(
-      (image: {
-        attributes: {
-          url: string;
-          alternativeText: string;
-        };
-      }) => ({
-        src:
-          `http://${process.env.NEXT_PUBLIC_CMS_HOST}` + image?.attributes?.url,
-        alt: image?.attributes?.alternativeText,
-      })
-    );
-
-  const fallbackImages = [
-    {
-      src: "/images/vision-pro.avif",
-      alt: "Apple Vision Pro",
-    },
-    {
-      src: "/images/vision-pro-teardown.avif",
-      alt: "Apple Vision Pro Teardown",
-    },
-  ];
-
-  return (
-    <main className="flex min-h-screen gap-16 flex-col">
-      <div>
-        <h1 className="max-w-md">{heading ?? fallbackHeading}</h1>
-        <p className="mt-2 max-w-xl">{subheading ?? fallbackSubheading}</p>
-      </div>
-      <section className=".cta">
-        <Button asChild>
-          <a href={ctaButtonUrl ?? fallbackCtaButtonUrl} target="_blank">
-            {ctaButtonTitle ?? fallbackCtaButtonTitle}
-          </a>
-        </Button>
-      </section>
-      <div className="flex flex-col gap-y-4">
-        <div className="max-w-xl">
-          <h1 className="mb-5">
-            <span>{bodyHeading ?? fallbackBodyHeading}</span>
-            <span className="text-green-500">
-              {bodySubheading ?? fallbackBodySubheading}
-            </span>
-          </h1>
-          <p>{bodyText ?? fallbackBodyText}</p>
-        </div>
-
-        <ol className="list-decimal mb-10 max-w-xl">
-          {bodyList ? (
-            bodyList.map(
-              (
-                listItem: {
-                  children: Array<{
-                    type: string;
-                    text: string;
-                  }>;
-                },
-                index: number
-              ) => {
-                const text = listItem?.children[0]?.text;
-                return <li key={index}>{text}</li>;
-              }
-            )
-          ) : (
-            <>
-              <li>
-                There had to be a considerable number of individuals / entities,
-                willing to share the knowledge within technology they had built
-                ( open source ).
-              </li>
-              <li>
-                There had to be a considerable number of individuals / entities
-                who are highly curious about this technology and want to tinker
-                with it, tear it down and create new things from it.
-              </li>
-              <li>
-                Problems and inadequacies in the current process of sharing and
-                tinkering with the open source knowledge.
-              </li>
-            </>
-          )}
-        </ol>
-
-        <ImageGroup images={images ?? fallbackImages} />
-      </div>
-    </main>
-  );
-};
 type HomeData = {
   attributes: {
     createdAt: string;
@@ -135,6 +21,84 @@ type HomeData = {
       landing_images: any;
     };
   };
+};
+
+const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const heading = data?.attributes?.heading;
+  const fallbackHeading =
+    "Tnkr.ai is a tool for exploring the source knowledge of any technology.";
+  const subheading = data?.attributes?.subheading;
+  const fallbackSubheading =
+    "We believe that every piece of technology, old or new, has a unique story within it. We want to help the next generation of tinkerers uncover these stories so new ones can be told.";
+
+  const ctaButtonTitle = data?.attributes?.cta?.button?.title;
+  const fallbackCtaButtonTitle = "Join Our Discord";
+  const ctaButtonUrl = data?.attributes?.cta?.button?.url;
+  const fallbackCtaButtonUrl = "https://discord.gg/fcpeKMKn3E";
+
+  const bodyHeading = data?.attributes?.landing_body?.heading;
+  const fallbackBodyHeading = "Robots";
+
+  const bodyText = data?.attributes?.landing_body?.body[0]?.children[0]?.text;
+  const fallbackBodyText =
+    "We've choosen the open-source robots as our first technology with these three conditions guiding us. Commonality, Curiosity, and Problems Solving.";
+
+  const images =
+    data?.attributes?.landing_body?.landing_images?.body_media?.data.map(
+      (image: {
+        attributes: {
+          url: string;
+          alternativeText: string;
+        };
+      }) => ({
+        src:
+          `http://${process.env.NEXT_PUBLIC_CMS_HOST}` + image?.attributes?.url,
+        alt: image?.attributes?.alternativeText,
+      })
+    );
+
+  const fallbackImages = [
+    {
+      src: "/videos/video-1.mp4",
+      alt: "Video 1",
+    },
+    {
+      src: "/videos/video-2.mp4",
+      alt: "Video 2",
+    },
+    {
+      src: "/videos/video-3.mp4",
+      alt: "Video 3",
+    },
+  ];
+
+  return (
+    <main className="flex min-h-screen gap-16 flex-col">
+      <div>
+        <h1 className="max-w-md">{heading ?? fallbackHeading}</h1>
+        <p className="mt-2 max-w-xl">{subheading ?? fallbackSubheading}</p>
+      </div>
+
+      <section className=".cta">
+        <Button asChild>
+          <a href={ctaButtonUrl ?? fallbackCtaButtonUrl} target="_blank">
+            {ctaButtonTitle ?? fallbackCtaButtonTitle}
+          </a>
+        </Button>
+      </section>
+
+      <div>
+        <div className="max-w-xl mb-5">
+          <h1 className="mb-2 w-full border-b py-2">
+            {bodyHeading ?? fallbackBodyHeading}
+          </h1>
+          <p>{bodyText ?? fallbackBodyText}</p>
+        </div>
+
+        <MediaGroup assets={images ?? fallbackImages} />
+      </div>
+    </main>
+  );
 };
 
 export const getStaticProps: GetStaticProps<{ data: HomeData }> = async () => {
@@ -167,10 +131,6 @@ export const getStaticProps: GetStaticProps<{ data: HomeData }> = async () => {
   return {
     props: data,
   };
-};
-
-Home.getLayout = (page: React.ReactElement) => {
-  return <Layout>{page}</Layout>;
 };
 
 export default Home;
