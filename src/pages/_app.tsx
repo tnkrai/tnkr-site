dc0004c5389853b2e49c20ca26761db47cab7776
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-06-21 14:57:08 +0100

    fix: import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 
-import Layout from "@components/layout";
+import Layout from "../components/layout";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
