ab00df8629af22494ca79e911275057467a25cda
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2024-06-25 16:43:56 +0100

    fix: header text (#27)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <header className="fixed w-full border-b bg-black py-3">
       <div className="flex justify-between items-center container">
-        <Link href="/">tnkr.ai</Link>
+        <Link href="/" className="text-white">
+          tnkr.ai
+        </Link>
 
         <Button asChild size="sm" variant="outline">
           <a href="https://discord.gg/fcpeKMKn3E" target="_blank">
